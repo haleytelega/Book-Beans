22b1822170cd60bed3c9dfa93ee4741a81c18ffd
Haley Telega <haleytelega@outlook.com>
2022-05-25 20:03:00 -0400

    trying to get my coffee route to work

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const coffeeRoutes = require('./coffee-routes.js');
+const coffeeRoutes = require('./coffee-routes');
 
 router.use('/coffee', coffeeRoutes);
 
