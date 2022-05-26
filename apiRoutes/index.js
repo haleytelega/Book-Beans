b01afa088835bb774ebc444ba67ad074905e4a1a
Shaun Sullivan <shaunsully88@gmail.com>
2022-05-26 19:31:46 -0400

    merged my API folder into Haley's API folder. book model and book-comment model created. Everything linked together

--- apiRoutes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const addRoutes = require('./bookRoutes/addRoutes');
-const commentRoutes = require('./bookRoutes/commentRoutes');
-
-router.use('/add', addRoutes);
-router.use('/comments', commentRoutes);
-
-module.exports = router;
