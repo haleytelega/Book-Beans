fa88cbb55a28d463af9ed58d3670934cb8f5deef
VSartz <elsartz@gmail.com>
2022-05-27 11:53:35 -0400

    add ready

---
+++ public/javascript/add-cafe.js
@@ -0,0 +1,25 @@
+async function newFormHandlerCafe(event) {
+    event.preventDefault();
+  
+    const cafe_name = document.querySelector('input[name="cafe-name"]').value;
+    const city_name = document.querySelector('input[name="city-name"]').value;
+  
+    const response = await fetch(`/api/cafes`, {
+      method: 'POST',
+      body: JSON.stringify({
+        cafe_name,
+        city_name
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('#new-post-form').addEventListener('submit', newFormHandlerCafe);
