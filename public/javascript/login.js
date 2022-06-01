7938079bcebfd56911b0f63c135d21c32a1f28f0
VSartz <elsartz@gmail.com>
2022-06-01 17:50:17 -0400

    closer i think

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -25,9 +25,9 @@ async function loginFormHandler(event) {
 async function signupFormHandler(event) {
   event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  const username = document.querySelector('#username-signup').value;
+  const email = document.querySelector('#email-signup').value
+  const password = document.querySelector('#password-signup').value
 
   if (username && email && password) {
     const response = await fetch('/api/users', {
@@ -41,13 +41,13 @@ async function signupFormHandler(event) {
     });
 
     if (response.ok) {
-      document.location.replace('/dashboard/');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
   }
 }
 
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('#login').addEventListener('submit', loginFormHandler);
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('#sign-up').addEventListener('submit', signupFormHandler);
